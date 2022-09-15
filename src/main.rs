b1060d490f6f3becc25dfac98869136c13754742
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2022-09-15 16:18:39 +0300

    [#1] Created a basic REPL loop that just echoes what you say back.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use std::io::{stdout, Write};
+use text_io::read;
+
+fn main() -> Result<()> {
+    loop {
+        print!("=>> ");
+        stdout().flush()?;
+
+        let user_input: String = read!("{}\n");
+        println!("The command was: [{}].", user_input);
+    }
 }
