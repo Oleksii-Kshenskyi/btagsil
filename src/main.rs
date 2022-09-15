18df7f5b572385fd43508f9d4f4c2efa8ec19728
Oleksii Kshenskyi <30338124+Oleksii-Kshenskyi@users.noreply.github.com>
2022-09-15 16:40:10 +0300

    [#1] A basic REPL for BTAGSIL (#5)
    * [#1] Created a basic REPL loop that just echoes what you say back.
    
    * [#1] Added the ability to split the string into tags and fixed Cargo.toml.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use std::io::{stdout, Write};
+use text_io::read;
+
+fn main() -> Result<()> {
+    loop {
+        print!("=>> ");
+        stdout().flush()?;
+
+        let user_input: String = read!("{}\n");
+        let tags = user_input
+            .split_whitespace()
+            .map(|s| s.to_owned())
+            .collect::<Vec<_>>();
+        println!("The command was: [{}].", user_input);
+        println!("The tags are: {:?}", tags);
+    }
 }
