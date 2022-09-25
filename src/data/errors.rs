00e139613a2c7d4f310ce2a502862cde579150c2
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2022-09-26 02:51:35 +0300

    [#7] [UNTESTED] The parser is finished and compiles, but is not used by the actual game yet.

--- src/data/errors.rs
+++ src/data/errors.rs
@@ -18,3 +18,68 @@ impl fmt::Display for ActionEmptyError {
         Ok(())
     }
 }
+
+#[derive(Debug)]
+pub struct RootActionUnknownError {
+    root: String,
+}
+impl Error for RootActionUnknownError {}
+impl fmt::Display for RootActionUnknownError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Uhhh... What does '{}'ing actually accomplish here?",
+            self.root
+        )
+    }
+}
+impl<'a> RootActionUnknownError {
+    pub fn new(root: String) -> Self {
+        Self { root }
+    }
+}
+
+#[derive(Debug)]
+pub struct DoesNotTakeDirectObjectError {
+    root: String,
+    direct_object: String,
+}
+impl Error for DoesNotTakeDirectObjectError {}
+impl fmt::Display for DoesNotTakeDirectObjectError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Hmmm... How would you '{}' a '{}'?",
+            self.root, self.direct_object
+        )
+    }
+}
+impl<'a> DoesNotTakeDirectObjectError {
+    pub fn new(root: String, direct_object: String) -> Self {
+        Self {
+            root,
+            direct_object,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct DoesNotAcceptInfixError {
+    root: String,
+    infix: String,
+}
+impl Error for DoesNotAcceptInfixError {}
+impl fmt::Display for DoesNotAcceptInfixError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Hmmm... How would you '{}' '{}' something?",
+            self.root, self.infix
+        )
+    }
+}
+impl<'a> DoesNotAcceptInfixError {
+    pub fn new(root: String, infix: String) -> Self {
+        Self { root, infix }
+    }
+}
