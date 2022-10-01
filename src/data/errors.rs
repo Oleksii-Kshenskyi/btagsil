8cc12c749342ceb2242edecd7853cb6872a95cf6
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2022-10-01 21:48:22 +0300

    [#10] Implemented the enum errors:
    - TODO: the enum errors are not wired up to the code;
    - TODO: the Error trait errors have not been removed yet;
    - TODO: I still do not know if the enum error handling is going to be viable or not.

--- src/data/errors.rs
+++ src/data/errors.rs
@@ -2,6 +2,58 @@ use std::error::Error;
 use std::fmt;
 
 #[derive(Debug)]
+enum ErrorType {
+    CLIUsage(CliError),
+    Parsing(ParsingError),
+}
+impl fmt::Display for ErrorType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::CLIUsage(x) => write!(f, "{}", x),
+            Self::Parsing(x) => write!(f, "{}", x),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum CliError {
+    ActionUnknown,
+    ActionEmpty,
+}
+impl fmt::Display for CliError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let message = match self {
+            Self::ActionUnknown => "No clue how to do that!",
+            Self::ActionEmpty => "",
+        };
+        write!(f, "{}", message)
+    }
+}
+
+#[derive(Debug)]
+enum ParsingError {
+    RootActionUnknown(String),
+    DoesNotTakeDirectObject(String, String),
+    DoesNotAcceptInfix(String, String),
+}
+impl fmt::Display for ParsingError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let message = match self {
+            Self::RootActionUnknown(r) => {
+                format!("Uhhh... What does '{}'ing actually accomplish here?", r)
+            }
+            Self::DoesNotTakeDirectObject(r, dobj) => {
+                format!("Hmmm... How would you '{}' a '{}'?", r, dobj)
+            }
+            Self::DoesNotAcceptInfix(r, inf) => {
+                format!("Hmmm... How would you '{}' '{}' something?", r, inf)
+            }
+        };
+        write!(f, "{}", message)
+    }
+}
+
+#[derive(Debug)]
 pub struct ActionUnknownError;
 impl Error for ActionUnknownError {}
 impl fmt::Display for ActionUnknownError {
