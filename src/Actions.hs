d141e2948a624f09c1d697be2e718905e0c094ea
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2022-10-10 00:54:21 +0300

    [#21] New "act" execution mechanism correctly decides what the application should do.

--- src/Actions.hs
+++ src/Actions.hs
@@ -1,10 +1,16 @@
 module Actions (
     execute,
-    exitMessage
+    exitMessage,
+    ControlFlow (..)
 ) where
 
 import Parse
 import qualified Data.Text as T
+
+data ControlFlow = EmptyResponse
+                 | TextResponse T.Text
+                 | ExitGame T.Text
+
 
 exitMessage :: T.Text
 exitMessage = T.pack "Thanks for playing!"
@@ -12,9 +18,10 @@ exitMessage = T.pack "Thanks for playing!"
 unknownMessage :: T.Text -> T.Text
 unknownMessage what = T.pack $ "I'm sorry, I don't know what '" ++ T.unpack what ++ "' is."
 
-execute :: Action -> Maybe T.Text
-execute action = case action of
-    Echo echoed -> Just echoed
-    Unknown what -> Just $ unknownMessage what
-    Exit -> Just exitMessage
-    Empty -> Nothing
+execute :: Action -> ControlFlow
+execute action =
+    case action of
+        Echo echoed -> TextResponse echoed
+        Unknown what -> TextResponse $ unknownMessage what
+        Exit -> ExitGame exitMessage
+        Empty -> EmptyResponse
--- src/Lib.hs
+++ src/Lib.hs
@@ -7,26 +7,17 @@ import qualified Data.Text.IO as I
 import qualified System.IO as SI
 import qualified System.Exit as E
 import qualified System.IO.Unsafe as U
-import qualified Control.Monad as C
+
 import Parse
 import Actions
 
-echo :: Maybe T.Text -> IO ()
-echo (Just x) = I.putStrLn (x <> T.pack "\n")
-echo Nothing = putStr ""
-
-strResponse :: Maybe T.Text -> T.Text
-strResponse (Just t) = t
-strResponse Nothing = T.pack ""
+act :: ControlFlow -> IO ()
+act EmptyResponse = I.putStr $ T.pack ""
+act (ExitGame message) = (I.putStrLn $ message <> T.pack "\n") >> E.exitWith E.ExitSuccess
+act (TextResponse response) = I.putStrLn (response <> T.pack "\n")
 
 readOnce :: T.Text
 readOnce = U.unsafePerformIO $ I.putStr (T.pack ">> ") >> SI.hFlush SI.stdout >> I.getLine
 
-exitGame :: IO ()
-exitGame = (I.putStrLn $ exitMessage <> T.pack "\n") >> E.exitWith E.ExitSuccess
-
 run :: IO ()
-run = if text == exitMessage then exitGame else (echo $ execute $ parse readOnce) >> run
-    where
-        response = execute $ parse readOnce
-        text = strResponse response
+run = (act $ execute $ parse readOnce) >> run
