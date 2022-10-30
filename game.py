9b6c02626465e5d84d2ec28d0fe3afe6a496c4b9
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2022-10-30 22:20:23 +0200

    [#23] Modified game structure: only the entry point file is now in the root folder. Others are in btagsil/.

--- game.py
+++ game.py
@@ -1,9 +1,9 @@
-from repl import repl_once
+from btagsil.repl import repl_once
 
 def main() -> None:
     while True:
         print("==> ", sep="", end="")
-        repl_once(input())
+        repl_once(input().lower())
 
 if __name__ == "__main__":
     main()
