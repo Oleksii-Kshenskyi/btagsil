503cb5e10f62f6506dcc268496d8b4b2250ec3d8
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2022-10-30 22:13:41 +0200

    [#23] Basic REPL with the traditional exit, echo, unknown and empty actions.

--- repl.py
+++ repl.py
@@ -1,3 +1,28 @@
+from sys import exit
+
+def repl_unknown(what: list[str]):
+    print(f"Wat? What's a '{' '.join(what)}'?")
+
+def repl_exit(args: list[str]) -> None:
+    match args:
+        case []:
+            print("Thanks for playing! Hope to see you again soon!\n")
+            exit(0)
+        case x: print(f"If you want to exit the game, you don't need any {' '.join(x)}s. Just write 'exit'.")
+
+def repl_echo(what: list[str]) -> None:
+    match what:
+        case []: print("What do you want to echo?")
+        case x: print(f"{' '.join(x)}")
+
+def repl_empty() -> None:
+    """This is an empty action that does nothing. Occurs when user inputs nothing (or spaces) and presses enter."""
+
 def repl_once(input: str) -> None:
     tags = input.split()
-    print(f"ECHOED: '{' '.join(tags)}'")
+    match tags:
+        case []: repl_empty()
+        case ["echo", *args]: repl_echo(args)
+        case ["exit", *lol]: repl_exit(lol)
+        case wtf: repl_unknown(wtf)
+    print() if tags else print(end="", sep="")
