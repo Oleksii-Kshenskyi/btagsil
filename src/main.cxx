43f1bc2cb3a77bd69f196304761010092f242bb2
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2023-03-18 13:50:41 +0200

    [#31] Dumb basic REPL without any architecture. A start.

--- src/main.cxx
+++ src/main.cxx
@@ -1,24 +1,13 @@
-#include <boost/hof.hpp>
-
+#include <string>
 #include <iostream>
 
-using namespace boost::hof;
-
-// A sum function object
-struct sum_f
-{
-    template<class T, class U>
-    auto operator()(T x, U y) const
-    {
-        return x + y;
-    }
-};
-
-BOOST_HOF_STATIC_FUNCTION(sum) = pipable(sum_f());
-
 int main() {
-    auto thirteen = 3 | sum(6) | sum(4);
-    std::cout << "Boost.HOF pipable: should be thirteen: " << thirteen << std::endl;
+    std::string user_input;
+    while(user_input != "exit") {
+        std::cout << "=>> ";
+        std::getline(std::cin, user_input);
+        std::cout << "Echoed: '" << user_input << "'" << std::endl << std::endl;
+    }
 
     return 0;
 }
